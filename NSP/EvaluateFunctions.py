4d198db479130b1d223b97a1227ffb308e0821fa
nishio <>
2018-07-05 01:01:51 +0900

    評価関数ファイルの追加

---
+++ NSP/EvaluateFunctions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 00:08:48 2018
+
+@author: toshiki
+"""
+
+
+def F1():  # 勤務パターン負荷の軽減
+    return 0
+
+
+def F2():  # 禁止パターンの低減
+    return 0
+
+
+def F3():  # 必要日数の確保
+    return 0
+
+
+def F4():  # 勤務間隔の均等化
+    return 0
+
+
+def F5():  # 必要人数の確保
+    return 0
+
+
+def F6():  # グループ人数の確保
+    return 0
+
+
+def F7():  # 勤務の質の確保
+    return 0
+
+
+def getFunctions():
+    return [F1, F2, F3, F4, F5, F6, F7]
--- NSP/NSP.py
+++ NSP/NSP.py
@@ -5,12 +5,15 @@ Created on Thu Jul  5 00:08:48 2018
 
 @author: toshiki
 """
-
-
-def printTest():
-    print("bbb")
+import EvaluateFunctions
 
 
 if __name__ == '__main__':
-    print("aaa")
-    printTest()
+    MAN = ["A", "B", "C", "D"]
+    DAY = ["07/01", "07/02", "07/03", "07/04", "07/05"]
+    WORK = ["日勤", "準夜勤", "夜勤"]
+    GROUP = ["バイト", "社員"]
+    # PROBLEM = []  # 未設定
+    PROBLEM = EvaluateFunctions.getFunctions()
+    for func in PROBLEM:
+        print(func, func())
