8730a6ec2aef002591d020c081092dfcf7f33e58
nishio <>
2018-07-05 00:19:44 +0900

    ファイル作成

---
+++ NSP/NSP.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 00:08:48 2018
+
+@author: toshiki
+"""
+
+
+def printTest():
+    print("bbb")
+
+
+if __name__ == '__main__':
+    print("aaa")
+    printTest()
