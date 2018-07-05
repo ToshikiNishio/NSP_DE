61cded5b2562e8b88d7760e4fe5a83300848a9a7
nishio <>
2018-07-05 23:47:45 +0900

    必要人数のデータ作成

--- NSP/NSP.py
+++ NSP/NSP.py
@@ -11,6 +11,7 @@ import EvaluateFunctions
 
 
 if __name__ == '__main__':
+    # パラメーターの設定
     MAN = ["A", "B", "C", "D"]
     DAY = ["07/01", "07/02", "07/03", "07/04", "07/05"]
     WORK = ["休み", "日勤", "準夜勤", "夜勤"]
@@ -20,6 +21,15 @@ if __name__ == '__main__':
     for func in PROBLEM:
         print(func, func())
 
+    requiredManNum = {
+        "07/01": {"休み": 4, "日勤": 2, "準夜勤": 0, "夜勤": 0},
+        "07/02": {"休み": 4, "日勤": 1, "準夜勤": 0, "夜勤": 1},
+        "07/03": {"休み": 3, "日勤": 1, "準夜勤": 1, "夜勤": 1},
+        "07/04": {"休み": 4, "日勤": 1, "準夜勤": 0, "夜勤": 1},
+        "07/05": {"休み": 4, "日勤": 2, "準夜勤": 0, "夜勤": 0},
+    }
+
+    # 表示する勤務表作成
     matrix = pd.DataFrame(np.random.rand(len(MAN), len(DAY)) * len(WORK),
                           index=MAN, columns=DAY)
     print(matrix)
