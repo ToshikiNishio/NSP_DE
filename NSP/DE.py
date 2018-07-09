4586b89c2bdaf33c426e1c12247713bc3b6f91a2
nishio <>
2018-07-09 18:30:03 +0900

    遺伝子データの構造変更

--- NSP/DE.py
+++ NSP/DE.py
@@ -43,7 +43,7 @@ class DE(object):
         print("DE initalize")
         pop = Population()
         print(pop)
-        pop.generateMutantParent()
+        # pop.generateMutantParent()
 
 
 class Population(object):
@@ -57,8 +57,8 @@ class Population(object):
         for i in range(NP):
             self.pop.append(Individual())
         print(self.pop)
-        print("max_fitness=", max(self.pop, key=lambda x: x.fitness).fitness)
-        print("min_fitness=", min(self.pop, key=lambda x: x.fitness).fitness)
+        # print("max_fitness=", max(self.pop, key=lambda x: x.fitness).fitness)
+        # print("min_fitness=", min(self.pop, key=lambda x: x.fitness).fitness)
 
     def generateMutantParent(self):  # 差分変異親個体vの生成
         print("generateDifferentialMutantParent")
@@ -73,7 +73,7 @@ class Individual(object):
     '''
     indivisual of DE
     '''
-    gene = None
+    gene = None #遺伝子には人を識別するためのインデックスが格納
     H1 = None
     H2 = None
     H3 = None
@@ -82,10 +82,28 @@ class Individual(object):
     def __init__(self):
         print("inidivisual initialize")
         # 個体の生成
-        gene = pd.DataFrame()
+        gene = pd.Series()
+        index = []
         for day in DAY:
-            series = pd.Series()
             # day日の遺伝子を作成
+            print(day)
+            series = pd.Series(np.arange(len(MAN)))
+            for work in WORK:
+                num = requiredManNum[day][work]
+                for n in range(num):
+                    # index.append(day + "-" + work)
+                    index.append((day, work))
+            np.random.shuffle(series)  # ランダムに並び替え
+            # gene = gene.append(series)
+            gene = pd.concat([gene, series])
+        index = pd.MultiIndex.from_tuples(index, names=['day', 'work'])
+        gene.index = index
+        self.gene = gene
+        print("gene=",)
+        print(self.gene)
+        # series.index = index
+        '''
+
             for index, work in enumerate(WORK):
                 num = requiredManNum[day][work]
                 series = pd.concat([series, pd.Series(np.ones(num) * index)])
@@ -97,6 +115,7 @@ class Individual(object):
         print(self.gene)
         self.fitness, self.H1, self.H2, self.H3 = calcFitness(self.gene)
         print(self.fitness, self.H1)
+        '''
 
     def generateMutantParent(self, parents):  # 差分変異親個体vの生成
         P1 = parents[0].gene.stack()
