81ec9949b1eaf8fbe0124d51047116f94e2e7c0a
nishio <>
2018-07-16 16:25:22 +0900

    交叉処理実装

--- NSP/DE.py
+++ NSP/DE.py
@@ -111,37 +111,22 @@ class Individual(object):
         print("P1,2,3")
         print(P1, P2, P3)
         mutantParent = P1 + S * (P2 - P3)
-        print("mutantParent")
-        print(mutantParent)
         for index, day in enumerate(DAY):
             st, end = index*len(MAN), index*(len(MAN)) + len(MAN)
             gene = mutantParent.iloc[st: end]
-            # print(gene)
-            # print("sort")
             argsort_list = np.argsort(gene)
-            # print(argsort_list)
             mutantParent.iloc[st: end] = argsort_list
-            # print("mutantParent.iloc[st: end]")
-            # print(mutantParent.iloc[st: end])
+        print("mutantParent")
         print(mutantParent)
-
-        """
-        # 上下限制約を満たすようにマッピング
-        mapFucn = lambda x: 0 if x < 0 else len(WORK)-1 if x > len(WORK)-1 else x
-        mutantParent = mutantParent.map(mapFucn)
-        mutantParent = np.round(mutantParent)  # 四捨五入で整数に変換
-        print("mutantParent 四捨五入")
-        print(mutantParent)
-        """
-        # self.createChild(mutantParent)
+        self.createChild(mutantParent)
 
     def createChild(self, mutantParent):
-        print("createChild")
+        print("createChild***********************")
         gene_len = len(mutantParent)
-        print("gene_len=", gene_len)
-        start = np.random.randint(gene_len)  # 0~len(mutantParent)-1
-        assert start < 15, "start >= 15!!!"
+        start = np.random.randint(gene_len)  # 交叉スタート位置 0~len(mutantParent)-1
         print("start=", start)
+        assert start < 15, "start >= 15!!!"
+        # 交叉する数を決定
         count = 1  # 交叉で交換する数
         while True:
             if np.random.rand() > Cr or count == gene_len:
@@ -156,80 +141,32 @@ class Individual(object):
             tmp1 = np.arange(start, gene_len)
             tmp2 = np.arange(0, start+count-gene_len)
             cross_idx = np.hstack((tmp1, tmp2))
-            
-        print("cross_idx")
-        print(cross_idx)
-        
-        new_gene = self.gene
-        for index, day in enumerate(DAY):
-            print(day, index*len(MAN), index*(len(MAN)) + len(MAN))
-            st, end = index*len(MAN), index*(len(MAN)) + len(MAN)
-            print(new_gene.iloc[st: end])
-            cross = []
-            notCross = []
-            for i in range(st, end):
-                cross.append(i in cross_idx)
-                notCross.append(i not in cross_idx)
-            cross_gene = new_gene.iloc[st: end][cross]
-            notCross_gene = new_gene.iloc[st: end][notCross]
-            print("cross=", cross)
-            print(cross_gene)
-            print("notCross=", notCross)
-            print(notCross_gene)
-            if len(cross_gene) == 0:
-                continue
-            
-            mutant_gene = mutantParent.iloc[st: end]
-            print("mutant_gene", mutant_gene)
-            for i in range(len(notCross_gene)):
-                print(i)
-            
-            #for i in range(st, end):
-                
-                
 
-
-        """
-        target = start
-        while count > 0:
-            target = target+1 if target < gene_len-1 else 0
-            print("target=", target)
-            count -= 1
-        """
-
-        """
-        if start+count < gene_len:
-            print("if")
-            notCross_series = self.gene.iloc[0: start+count-gene_len]
-            notCross_series = pd.concat([notCross_series,
-                                         self.gene.iloc[start: start+count-gene_len]])
-        else:
-            print("else")
-            notCross_series = self.gene.iloc[0: start]
-        print("gene=")
-        print(self.gene)
-        print("notCross_series")
-        print(notCross_series)
-        """
-        
-        """
+        # 交叉処理
+        new_gene = self.gene  # 自身の遺伝子をコピー
+        print("new_gene")
+        print(new_gene)
         print("mutantParent")
-        print(mutantParent.unstack())
-        print("mutantParent len=", len(mutantParent))
-        start = np.random.randint(len(mutantParent))
-        print("start=", start)
-        count = 1  # 交叉で交換する数
-        while True:
-            if np.random.rand() > Cr or count == len(mutantParent):
-                break
-            else:
-                count += 1
-        print("count=", count)
-        gene = self.gene.stack()
-        print("gene=", gene)
-        print(gene.iloc[start: start+count])
-        # child = gene
-        # child.iloc[start: start+count] = mutantParent.iloc[start: start+count]
-        # print("child=", child)
-        print("----------------------------------------------------------")
-        """
+        print(mutantParent)
+        for index, day in enumerate(DAY):  # 日付で分割して交叉処理を行う
+            st = index * len(MAN)  # 日付の初めのインデックス
+            end = index*(len(MAN)) + len(MAN)  # 日付の終わりのインデックス
+            # print(new_gene.iloc[st: end])
+            cross_idx_day = []  # 交叉処理を行うインデックス
+            cross = []
+            for i in range(st, end):
+                if i in cross_idx:
+                    cross_idx_day.append(i)
+                cross.append(i in cross_idx)
+            if len(cross_idx_day) == 0:  # 交叉しなければスキップ
+                continue
+            cross_gene = new_gene.iloc[st: end][cross]
+
+            mutant_gene = mutantParent.iloc[st: end]
+            # 差分変異親個体で交叉対象の遺伝子を抜き出す
+            cross_pool = mutant_gene[mutant_gene.isin(cross_gene)]
+            for count, ind in enumerate(cross_idx_day):
+                new_gene.iat[ind] = cross_pool.iat[count]
+        print("new_gene")
+        print(new_gene)
+        print("********************************")
